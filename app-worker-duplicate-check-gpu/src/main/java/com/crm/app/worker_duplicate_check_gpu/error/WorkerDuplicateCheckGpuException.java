06c48a657b246236149eae6a663f2ed0f7eed5e7
ralf <ralf@ralfscholler.de>
2025-12-11 21:25:02 +0100

    Updated

---
+++ app-worker-duplicate-check-gpu/src/main/java/com/crm/app/worker_duplicate_check_gpu/error/WorkerDuplicateCheckGpuException.java
@@ -0,0 +1,7 @@
+package com.crm.app.worker_duplicate_check_gpu.error;
+
+public class WorkerDuplicateCheckGpuException extends RuntimeException {
+    public WorkerDuplicateCheckGpuException(String message) {
+        super(message);
+    }
+}
