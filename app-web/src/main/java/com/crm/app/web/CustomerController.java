50f4d90610c224e0915f3e847b34b1d0694068e6
ralf <ralf@ralfscholler.de>
2025-12-12 10:30:12 +0100

    Updated

--- app-web/src/main/java/com/crm/app/web/CustomerController.java
+++ app-web/src/main/java/com/crm/app/web/CustomerController.java
@@ -6,11 +6,9 @@ import com.crm.app.web.duplicate_check.DuplicateCheckService;
 import com.crm.app.web.error.CustomerNotFoundException;
 import com.crm.app.web.upload.CrmUploadService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
