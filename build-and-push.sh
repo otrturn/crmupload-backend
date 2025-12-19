d314c6f58bb73d284fd0e5ebf69356b8d3bacd6b
ralf <ralf@ralfscholler.de>
2025-12-19 23:55:45 +0100

    Updated

--- build-and-push.sh
+++ build-and-push.sh
@@ -26,8 +26,14 @@ fi
 echo ">>> Build crmupload-web (Maven)"
 mvn -pl app-web -am clean package -DskipTests
 
-echo ">>> Build crmupload-worker (Maven)"
+echo ">>> Build crmupload-worker-upload (Maven)"
 mvn -pl app-worker-upload -am clean package -DskipTests
+
+echo ">>> Build crmupload-worker-duplicate-check (Maven)"
+mvn -pl app-worker-duplicate-check -am clean package -DskipTests
+
+echo ">>> Build crmupload-billing (Maven)"
+mvn -pl app-billing -am clean package -DskipTests
 
 # -------------------------------------------------------------------
 # Docker Login bei GHCR
@@ -54,12 +60,19 @@ docker buildx build \
   app-worker-upload/ \
   --push
 
-
 echo ">>> Docker build+push crmupload-worker-duplicate-check (multi-arch)..."
 docker buildx build \
   --platform linux/amd64,linux/arm64/v8 \
   -t ghcr.io/${GHCR_USER}/crmupload-worker-duplicate-check:prod \
   -f app-worker-duplicate-check/Dockerfile \
   app-worker-duplicate-check/ \
+  --push
+
+echo ">>> Docker build+push crmupload-billing (multi-arch)..."
+docker buildx build \
+  --platform linux/amd64,linux/arm64/v8 \
+  -t ghcr.io/${GHCR_USER}/crmupload-billing:prod \
+  -f app-billing/Dockerfile \
+  app-billing/ \
   --push
 
