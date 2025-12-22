267714e4d9222b7d7cb00f3bb322b00e3bd7c678
ralf <ralf@ralfscholler.de>
2025-12-22 11:22:17 +0100

    Updated

--- app-backend-e2e-tests/src/test/resources/db/prepare.sql
+++ app-backend-e2e-tests/src/test/resources/db/prepare.sql
@@ -1,1 +1,11 @@
-SET search_path TO app, public;
+TRUNCATE TABLE app.customer_invoice;
+TRUNCATE TABLE app.duplicate_check_observation;
+TRUNCATE TABLE app.duplicate_check;
+TRUNCATE TABLE app.crm_upload_observation;
+TRUNCATE TABLE app.crm_upload;
+TRUNCATE TABLE app.customer_product;
+TRUNCATE TABLE app.customer_activation;
+TRUNCATE TABLE app.customer_acknowledgement;
+TRUNCATE TABLE app.customer;
+TRUNCATE TABLE app.user_account;
+
